a6902d3c200f4755a497e59e633c8c6db21695c6
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-11-25 04:01:51 +0100

    creo el arbol de huffman con los nodos

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,16 +1,16 @@
 class nodoarbol:
 
-    def __init__(self, simbolo, frecuencia):
+    def __init__(self, caracter, frecuencia):
         self.izquierda = None
         self.derecha = None
         self.padre = None
-        self.simbolo = simbolo
+        self.caracter = caracter
         self.frecuencia = frecuencia
 
 def buscar(raiz, clave):
     posicion = None
     if raiz is not None:
-        if raiz.simbolo == clave:
+        if raiz.caracter == clave:
             posicion = raiz
             return posicion
         if posicion == None:
@@ -18,4 +18,29 @@ def buscar(raiz, clave):
         if posicion == None:
             posicion = buscar(raiz.derecha, clave)
     return
-    
+
+def ordenarobjetos(lista):
+    lista = sorted(lista, key=lambda x: x.caracter)
+    lista = sorted(lista, key=lambda x: x.frecuencia)
+    return lista
+
+def meternodoalarbol(lista, nodo):
+    for i in range(len(lista)):
+        if lista[i].frecuencia > nodo.frecuencia:
+            lista.insert(i, nodo)
+            break
+        if i == len(lista)-1:
+            lista.append(nodo)
+            break
+    return lista
+
+def arboldehuffman(caracteres, frecuencias):
+    nodos = []
+    for i in range(len(caracteres)):
+        nodos.append(nodoarbol(caracteres[i], frecuencias[i]))
+    nodos = ordenarobjetos(nodos)
+    while len(nodos)>1:
+        nodo = nodoarbol('XX', nodos[0].frecuencia + nodos[1].frecuencia)
+        nodo.izquierda = nodos[0]
+        nodo.izquierda.padre = nodo
+        nodo.derecha = nodos[1]
