7dce8d73ce047672cbed1c34b6453f26dcb57f04
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-11-25 05:15:36 +0100

    creo el main y compruebo si funciona

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -91,3 +91,20 @@ def descomprimirtexto(codigo, raiz):
     
     mensaje.append(nodo.caracter)
     return ''.join(mensaje)
+
+
+def main1():
+    caracteres = ['A', 'F', '1', '3', '0', 'M', 'T']
+    frecuencias = [0.2, 0.17, 0.13, 0.21, 0.05, 0.09, 0.15]
+    raiz = arboldehuffman(caracteres, frecuencias)
+    print("al comprimir cada caracter obtendremos el siguiente codigo: ")
+    print("Para la A: ", comprimirtexto('A', raiz))
+    print("Para la F: ", comprimirtexto('F', raiz))
+    print("Para el 1: ", comprimirtexto('1', raiz))
+    print("Para el 3: ", comprimirtexto('3', raiz))
+    print("Para el 0: ", comprimirtexto('0', raiz))
+    print("Para la M: ", comprimirtexto('T', raiz))
+    print("Para la T: ", comprimirtexto('M', raiz))
+    print("por ejemplo al codificar la palabra M0T0MAM1 obtenemos el siguiente codigo: ", comprimirtexto('M0T0MAM1', raiz))
+
+main1()
