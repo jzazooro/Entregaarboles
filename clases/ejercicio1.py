50f635ee2ae66db658136d66e7864417b743c1a9
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-11-25 04:33:53 +0100

    comprimo el menaje que le metas

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -44,3 +44,24 @@ def arboldehuffman(caracteres, frecuencias):
         nodo.izquierda = nodos[0]
         nodo.izquierda.padre = nodo
         nodo.derecha = nodos[1]
+        nodo.derecha.padre = nodo
+        nodos = meternodoalarbol(nodos, nodo)
+        nodos.pop(1)
+        nodos.pop(0)
+    return nodos[0]
+
+def comprimirtexto(mensaje, raiz):
+    codigo=[]
+    mensaje = mensaje[::-1]
+    for m in mensaje:
+        nodo = buscar(raiz, m)
+        while nodo.padre is not None:
+            if nodo.padre.izquierda == nodo:
+                codigo.append('0')
+            else: 
+                codigo.append('1')
+            nodo = nodo.padre
+    codigo = codigo[::-1]
+    return ''.join(codigo)
+
+    
